d07d151c715e7a24466060d6ae72ee9ca658513a
Roquebajosexto <roquebajosexto@gmail.com>
2020-01-30 23:58:16 -0600

    Mi primer commit

---
+++ por-hacer/por-hacer.js
@@ -0,0 +1,65 @@
+const fs = require('fs')
+
+let listadoPorHacer = [];
+
+const guardarDB = () => {
+    let data = JSON.stringify(listadoPorHacer);
+    fs.writeFile(`db/data.json`, data, (err) => {
+        if (err)
+            throw new Error(err)
+        else
+            console.log(`Guardado con exito data.json`)
+    });
+}
+const cargarDB = () => {
+    try {
+        listadoPorHacer = require('../db/data.json')
+    } catch (error) {
+        listadoPorHacer = []
+    }
+
+}
+const crear = (descripcion) => {
+    cargarDB();
+    let porHacer = {
+        descripcion,
+        completado: false
+    }
+    listadoPorHacer.push(porHacer)
+    guardarDB()
+    return porHacer
+}
+
+const getListado = () => {
+    cargarDB()
+    return listadoPorHacer
+
+}
+
+const actualizar = (descripcion, completado = true) => {
+    cargarDB()
+    let index = listadoPorHacer.findIndex(tarea => tarea.descripcion === descripcion)
+    if (index >= 0) {
+        listadoPorHacer[index].completado = completado
+        guardarDB()
+        return true
+    } else {
+        return false
+    }
+}
+
+const borrar = (descripcion, completado = true) => {
+    cargarDB()
+    let nuevListado = listadoPorHacer.filter(tarea => tarea.descripcion !== descripcion)
+
+    if (nuevListado.length === listadoPorHacer.length) {
+
+        return false
+    } else {
+        listadoPorHacer = nuevListado
+        guardarDB()
+        return true
+    }
+}
+
+module.exports = { crear, getListado, actualizar, borrar }
